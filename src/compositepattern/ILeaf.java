d9c3e7744b790cea66917aebbcdd2d7723567866
diyinqianchang <zhangguolin-1990@163.com>
2016-06-12 12:11:11 +0800

    模式

---
+++ src/compositepattern/ILeaf.java
@@ -0,0 +1,6 @@
+package compositepattern;
+
+public interface ILeaf {
+
+	public String getInfo();
+}
---
+++ src/compositepattern/IRoot.java
@@ -0,0 +1,11 @@
+package compositepattern;
+
+import java.util.ArrayList;
+
+public interface IRoot {
+
+	public String getInfo();
+	public void add(IBranch branch);
+	public void add(ILeaf leaf);
+	public ArrayList getSubordinateInfo();
+}
